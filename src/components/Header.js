6631f3ed07f86c33c44eb3ab041d12fa1db36281
Khushbakht Khan <103233325+khushbakhtkhan1@users.noreply.github.com>
2023-06-22 12:19:59 +0500

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = ({setIsAdding}) => {
+  return (
+    <header>
+        <h1 style={{textAlign:'center'}}>EMPLOYEE MANAGEMENT SYSTEM</h1>
+        <div style={{marginTop:'40px',marginBottom:'16px',textAlign:'center'}}>
+            <button onClick={()=>setIsAdding(true)} className='round-button'>Add Employee</button>
+        </div>
+    </header>
+  )
+}
+
+export default Header
