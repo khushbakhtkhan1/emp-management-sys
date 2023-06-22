0c41180af486addaf8de85ff3512eb34594e9ff6
Khushbakht Khan <103233325+khushbakhtkhan1@users.noreply.github.com>
2023-06-22 13:38:23 +0500

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = ({setIsAdding}) => {
   return (
     <header>
-        <h1 style={{textAlign:'center'}}>EMPLOYEE MANAGEMENT SYSTEM</h1>
+        <h1 style={{textAlign:'center',color:'white'}}>EMPLOYEE MANAGEMENT SYSTEM</h1>
         <div style={{marginTop:'40px',marginBottom:'16px',textAlign:'center'}}>
             <button onClick={()=>setIsAdding(true)} className='round-button'>Add Employee</button>
         </div>
